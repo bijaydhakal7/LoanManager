65062b827c3c67d4899447ffd2ad085f0aa6825d
bijaydhakal7 <dhakalbijay55@gmail.com>
2026-07-26 17:15:04 +0545

    fix: fix config error

--- client/lib/config.ts
+++ client/lib/config.ts
@@ -1,6 +1,6 @@
 export const env = {
   apiUrl: process.env.NEXT_PUBLIC_API_URL ,
-  csrfCookieName: process.env.NEXT_PUBLIC_CSRF_COOKIE_NAME ,
-  csrfHeaderName: process.env.NEXT_PUBLIC_CSRF_HEADER_NAME ,
+  csrfCookieName: process.env.NEXT_PUBLIC_CSRF_COOKIE_NAME as string ,
+  csrfHeaderName: process.env.NEXT_PUBLIC_CSRF_HEADER_NAME as string,
   currency: process.env.NEXT_PUBLIC_CURRENCY ?? "NPR",
 };

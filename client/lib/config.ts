4ba1170b300754743e2ecdbc163ca0d035d23e85
bijaydhakal7 <dhakalbijay55@gmail.com>
2026-07-26 16:42:31 +0545

    chore: manage secret variables

--- client/lib/config.ts
+++ client/lib/config.ts
@@ -1,6 +1,6 @@
 export const env = {
-  apiUrl: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5000/api",
-  csrfCookieName: process.env.NEXT_PUBLIC_CSRF_COOKIE_NAME ?? "csrfToken",
-  csrfHeaderName: process.env.NEXT_PUBLIC_CSRF_HEADER_NAME ?? "x-csrf-token",
+  apiUrl: process.env.NEXT_PUBLIC_API_URL ,
+  csrfCookieName: process.env.NEXT_PUBLIC_CSRF_COOKIE_NAME ,
+  csrfHeaderName: process.env.NEXT_PUBLIC_CSRF_HEADER_NAME ,
   currency: process.env.NEXT_PUBLIC_CURRENCY ?? "NPR",
 };
